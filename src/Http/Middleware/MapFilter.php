ceee7e75eb3f47b6c4abbbea432c9ead892fb21e
Андрей <info@bustep.ru>
2022-11-28 08:51:44 +0700

    job version

--- src/Http/Middleware/MapFilter.php
+++ src/Http/Middleware/MapFilter.php
@@ -9,11 +9,14 @@ use Symfony\Component\HttpKernel\Event\ControllerEvent as Event;
 
 class MapFilter implements Middleware
 {
-
     public function handle(Controller $controller, Request $request, Event $event): void
     {
         //  Загрука полей с которыми предстоит работать
-        \Ela\Facades\Map::create(getenv('ES_FIELDS_PATH'), $request);
+        $path = getenv('ES_SETTINS_PATH') . 'fields.yaml';
+        if (!file_exists($path)) {
+            throw new \Exception('no file fields ' . $path);
+        }
+        \Ela\Facades\Map::create($path, $request);
     }
 
 }
